31d2befdf517398df5233f10eaddd38839172376
Guilherme Dantas <gui.dantasp21@gmail.com>
2022-10-22 06:53:41 -0300

    feat: add basic board game functionalities on Battleship class
    Create an empty board, setup the board with all the elements of board array and print the game board

--- main.py
+++ main.py
@@ -4,21 +4,75 @@ from support import *
 class Battleship:
     def __init__(self, size=SIZE["default"]) -> None:
         
-        # board size 
+        # BOARD --------------------
+
+        # size 
         if self.is_size_valid(size):
             self.SIZE = size
         else:
             self.SIZE = SIZE["default"]
 
-    def is_size_valid(self, size):
+        # grid character -> each node of the grid is represented by a character
+        self.grid_char = colored(GRID["color"], GRID["char"])
+
+        # create board
+        self.setup_board()
+
+
+    def is_size_valid(self, size) -> bool:
         "Check if the passed size is valid"
 
         return SIZE["min"] <= size <= SIZE["max"]
 
-    def run(self):
+
+    def setup_board(self) -> None:
+        "Add all the elements on the game board"
+
+        # coord letters -> cols: letters (A,B,C,...) rows: numers (1,2,3..) 
+        self.coord_letters = ALPHA[0 : self.SIZE]
+
+        # create empty board
+        self.board = self.empty_board()
+
+    
+    def empty_board(self) -> list:
+        "Create a 2d list that represents the game board empty"
+
+        return [[self.grid_char for j in range(self.SIZE)] for i in range(self.SIZE)]
+
+    
+    def print_board(self) -> None:
+        "Print the game board on the terminal"
+
+        # colored row coord indicators -> A, B, C, D ...
+        print(' ', end="    ")
+        for l in self.coord_letters:
+            print(colored(GRID["coord color"], l), end="    ")
+        
+        print("\n")
+
+        # colored row coord inidcators -> 1, 2, 3, 4 ...
+        for n,i in enumerate(range(self.SIZE)):
+            print(colored(GRID["coord color"], n+1), end="    ")
+
+            # each element of the board
+            for j in range(self.SIZE):
+                print(self.board[i][j], end="    ")
+
+            print("\n")
+
+
+    def run(self) -> None:
         "Execute the game"
 
-        print(self.SIZE)
+        while True:
+            clean_terminal()
+
+            debug("Board size", self.SIZE)
+            debug("Grid character", self.grid_char)
+
+            self.print_board()
+            
 
 
 
--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-DEBUG = False
+DEBUG = True
 
 CLEAN_TERMINAL = True
 
