e3792a51ffd17411bd6c2ee863f4135357cd713d
Guilherme Dantas <gui.dantasp21@gmail.com>
2022-10-21 21:17:49 -0300

    feat: implement the initial settings on the setting.py

---
+++ settings.py
@@ -0,0 +1,48 @@
+DEBUG = False
+
+CLEAN_TERMINAL = True
+
+ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+SIZE = {
+    "default": 9,
+    "max": 26,
+    "min": 6
+}
+
+MESSAGES = {
+    "victory": "VICTORY!",
+    "defeat": "DEFEAT!"
+}
+
+COLORS = {
+    "red": (255,0,0),
+    "green": (0,255,0),
+    "blue": (0,0,255),
+    "light blue": (0,128,128),
+    "white": (255,255,255)
+}
+
+SHIP = {
+    "color": COLORS["blue"],
+    "char": "S",
+    "total shots": 10,
+}
+
+GRID = {
+    "color": COLORS["white"],
+    "coord color": COLORS["green"],
+    "char": "+"
+}
+
+ATTACK = {
+    "color": COLORS["light blue"],
+    "char": "X"
+}
+
+ENEMY = {
+    "color": COLORS["red"],
+    "char": "E",
+    "high weight": 5,
+    "normal weight": 1
+}
