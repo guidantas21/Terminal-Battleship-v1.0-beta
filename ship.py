7b2e6e99622c98e83fa50c377ebd5fb251bbed84
Guilherme Dantas <gui.dantasp21@gmail.com>
2022-10-23 21:07:24 -0300

    fix: verify if the length of the input attack is greater than 2
    When user inputs only one character, the game crashes

--- ship.py
+++ ship.py
@@ -25,19 +25,23 @@ class Ship:
 
         while True:
             # get user first two char of uppercase user input
-            coord = str(input(SHIP["messages"]["input attack coord"])).upper()[0:2].strip()
+            coord = str(input(SHIP["messages"]["input attack coord"])).upper().strip()
 
-            # check format "2A" (letter before number) and if it is on the board
-            if coord[0].isdigit() and not coord[1].isdigit():
-                if 0 < int(coord[0]) < self.SIZE and coord[1] in self.coord_letters:
-                    return coord[::-1]
+            if len(coord) >= 2:
+                coord = coord[0:2]
 
-             # check format "A2" (number before letter) and if it is on the board
-            elif not coord[0].isdigit() and coord[1].isdigit():
-                if 0 < int(coord[1]) < self.SIZE and coord[0] in self.coord_letters:
-                    return coord
+                # check format "2A" (letter before number) and if it is on the board
+                if coord[0].isdigit() and not coord[1].isdigit():
+                    if 0 < int(coord[0]) < self.SIZE and coord[1] in self.coord_letters:
+                        return coord[::-1]
 
-            print(colored(COLORS["red"],SHIP["messages"]["invalid input"]))
+                # check format "A2" (number before letter) and if it is on the board
+                elif not coord[0].isdigit() and coord[1].isdigit():
+                    if 0 < int(coord[1]) < self.SIZE and coord[0] in self.coord_letters:
+                        return coord
+                        
+            else:
+                print(colored(COLORS["red"],SHIP["messages"]["invalid input"]))
 
 
     def attack(self) -> tuple:
